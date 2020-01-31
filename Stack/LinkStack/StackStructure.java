c6c113812093192421d85e721819d0746bf687e3
H-Always <1790199849@qq.com>
2020-01-31 17:31:37 +0800

    顺序栈结构

--- Stack/LinkStack/StackStructure.java
+++ Stack/LinkStack/StackStructure.java
@@ -5,22 +5,25 @@
  * @date: 2020/1/25/14:11
  * @Version: 1.0
  */
+// 在JAVA中也有这自己的栈数据结构
+// 功能也非常的全，继承于Vector集合
+/*
+    Stack<Integer> stack = new Stack<>();
+ */
+
 public class StackStructure <T> {
 
     static final int SIZE = 1024;
-
-    // 顺序栈用数组实现
-    private Object[] stack;
-    // 栈的最大容量
-    private int Size;
 
     public StackStructure() {
         this(SIZE);
     }
 
     public StackStructure(int size) {
-        this.Size = 0;
-        this.stack = new Object[size];
+        // 栈的最大容量
+        int size1 = 0;
+        // 顺序栈用数组实现
+        Object[] stack = new Object[size];
     }
 
 }
