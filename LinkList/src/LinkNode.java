c94483a67944d3e31056700239023bfdc1fdf746
H-Always <1790199849@qq.com>
2020-03-10 07:58:28 +0800

    链表实现

--- LinkList/src/LinkNode.java
+++ LinkList/src/LinkNode.java
@@ -5,16 +5,7 @@
  * @date: 2020/1/23/0:21
  * @Version: 1.0
  */
-//一个链表类
-//对于链表来说，下面这几行注释大概就是最简单的链表类了吧
-//这三行完全可以构成一个简单的单链表用来实现算法相关
-/**
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
 public class LinkNode <T> {
 
     //定义链表的头结点
