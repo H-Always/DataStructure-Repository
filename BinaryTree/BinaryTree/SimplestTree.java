a98cdc80f2c257a0c95dbe1020bb9c61478d8417
H-Always <1790199849@qq.com>
2020-03-10 07:58:03 +0800

    最简单二叉树

---
+++ BinaryTree/BinaryTree/SimplestTree.java
@@ -0,0 +1,20 @@
+/**
+ * @ClassName: SimplestTree
+ * @Description: 最简单二叉树
+ * @Author: 余霜
+ * @Date: 2020/03/10 07:54
+ * @Version: V1.0
+ **/
+
+//最简单的二叉树类，写算法的时候可以用这个
+
+
+public class SimplestTree {
+    int val;
+    SimplestTree left;
+    SimplestTree right;
+
+    SimplestTree(int x) {
+        val = x;
+    }
+}
