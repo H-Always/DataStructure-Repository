0ebb50468fda8b7910c9d0009df734b019aeb555
H-Always <1790199849@qq.com>
2020-03-10 08:01:25 +0800

    最简单链表

---
+++ LinkList/src/SimplestList.java
@@ -0,0 +1,21 @@
+/**
+ * @ClassName: SimplestListNode
+ * @Description: 最简单的链表
+ * @Author: 余霜
+ * @Date: 2020/03/10 07:50
+ * @Version: V1.0
+ **/
+
+//一个链表类
+//对于链表来说，下面这几行注释大概就是最简单的链表类了吧
+//这三行完全可以构成一个简单的单链表用来实现算法相关
+
+
+
+public class SimplestList {
+    int val;
+    SimplestList next;
+    SimplestList(int x) {
+        val = x;
+    }
+}
